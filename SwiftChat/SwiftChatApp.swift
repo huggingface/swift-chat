56720d44ab623157e6eb53c4d5d86c663b77998f
Pedro Cuenca <pedro@huggingface.co>
2023-05-03 17:01:39 +0200

    Initial Commit

---
+++ SwiftChat/SwiftChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChatApp.swift
+//  SwiftChat
+//
+//  Created by Pedro Cuenca on 3/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftChatUITests/SwiftChatUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftChatUITestsLaunchTests.swift
+//  SwiftChatUITests
+//
+//  Created by Pedro Cuenca on 3/5/23.
+//
+
+import XCTest
+
+final class SwiftChatUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
